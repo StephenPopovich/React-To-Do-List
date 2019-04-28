cbba8d439ff801051405a31729fdb67947206174
StephenPopovich <stephenpopovich16@gmail.com>
2019-04-27 19:29:44 -0700

    add propTypes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
   render() {
     return (
       <div>
-        <p>Hello</p>
+        <p>{this.props.todo.title}</p>
       </div>
     )
   }
 }
+
+TodoItem.propTypes = {
+    todos: PropTypes.object.isRequired
+  }
+ 
 
 export default TodoItem
